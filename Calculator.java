9aded4945ac462f329c636f2e3f0b4406562a6f9
nayoseb <nayoseb1018@gmail.com>
2023-08-28 18:28:11 +0900

    #2023-git./1 불필요한 스캐너 변수 삭제

--- Calculator.java
+++ Calculator.java
@@ -4,7 +4,6 @@ public class Calculator {
     public static void main(String[] args) {
         System.out.println("===Java Calculator===");
 
-        Scanner input = new Scanner(System.in);
         /*
             요구 사항에 따라 간단한 계산기를 만들어주세요.
             1. 사용자의 입력으로 첫 번째 숫자, 연산자, 두 번째 숫자를 받아야 합니다.
