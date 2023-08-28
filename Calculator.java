7593b9ae1dc1d7e697194746b7631fca5217785d
nayoseb <nayoseb1018@gmail.com>
2023-08-28 17:59:47 +0900

    #2023-git./1 스켈레톤 작성.

---
+++ Calculator.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("input : ");
+        System.out.println("result : "); 
+    }
+
+    public static int add(int a, int b) {
+        return 0;
+    }
+
+    public static int sub(int a, int b) {
+        return 0;
+    }
+
+    public static int div(int a, int b) {
+        return 0;
+    }
+
+    public static int multi(int a, int b) {
+        return 0;
+    }
+}
